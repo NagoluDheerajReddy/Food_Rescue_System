edb48deaf47cba595d845f6f33b7378a06fe5600
Nagolu Dheeraj Reddy <137262067+NagoluDheerajReddy@users.noreply.github.com>
2026-03-25 02:24:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
-require("./config/db");
+require("./config/db"); // MongoDB connection is here
 
 const donationRoutes = require("./routes/donationRoutes");
 const matchingRoutes = require("./routes/matchingRoutes");
@@ -9,17 +10,26 @@ const pickupRoutes = require("./routes/pickupRoutes");
 
 const app = express();
 
+/* Middlewares */
 app.use(cors());
 app.use(express.json());
 
+/* Serve frontend */
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+/* API Routes */
 app.use("/api", donationRoutes);
 app.use("/api", matchingRoutes);
 app.use("/api", pickupRoutes);
 
-app.get("/", (req,res)=>{
-    res.send("Food Rescue API Running");
+/* Home Route */
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
-app.listen(5000, ()=>{
-    console.log("Server running on port 5000");
+/* Start Server */
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
