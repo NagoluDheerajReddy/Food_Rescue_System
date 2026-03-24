791145c096537de227d534cefdf3e850eda8258d
Nagolu Dheeraj Reddy <137262067+NagoluDheerajReddy@users.noreply.github.com>
2026-03-25 02:24:17 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/food_rescue")
-.then(()=>{
+mongoose.connect("mongodb+srv://admin:admin123@foodrescuecluster.hw6hwnm.mongodb.net/foodrescueDB")
+.then(() => {
     console.log("MongoDB Connected Successfully");
 })
-.catch(err => console.log(err));
-
-module.exports = mongoose;
+.catch((err) => {
+    console.log("MongoDB Connection Error:", err);
+});
